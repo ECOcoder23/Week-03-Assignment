6ff7764441e9c7e6b002fa6f4bba666f95f44768
ECOcoder23 <eobiozor@luc.edu>
2025-06-26 18:18:36 +0000

    Corrections

--- week3solutions.py
+++ week3solutions.py
@@ -4,20 +4,21 @@ def draw_top_portion(height):
     fill_order_list = list(range(1, height + 2, 1))
     for i in range(len(fill_order_list)):
         if (i % 2) == 1 and i < height + 2: 
-            print("#" * (sum + i), " " * (height - (sum + i)))
+           fill_line = "#" * (sum + i)
+           print(fill_line.center(height))
 
 def draw_bottom_portion(height):
-    sum = 0 
-    reverse_fill_order_list = list(range(height, 1, -1))
-    for i in range(len(reverse_fill_order_list)):
+    fill_order_list = list(range(0, height + 2, 1))
+    for i in range(len(fill_order_list)):
         if (i % 2) == 1 and i > 0:
-            print(" " * (height - (sum + i)), "#" * (sum + i))
+            fill_line = "#" * ((height + 1) - i)
+            print(fill_line.center(height))
 
 def draw_diamond(height):
     draw_top_portion(height)
     draw_bottom_portion(height) 
 
-draw_diamond(11)
+draw_diamond(13)
 
 #Draw Right Triangle
 def draw_right_triangle(height):
@@ -30,13 +31,10 @@ def draw_right_triangle(height):
 draw_right_triangle(13)
 
 #Compound Interest
-def compound_interest(years: range, principal:int, percentage:int): 
-   i = 0
-   while i < len(range(years)): 
-        if i < years:
-            new_total = principal + ((percentage/100) * principal)
-            return new_total 
-   i = i + 1
+def compound_interest(years: int, principal:int, percentage:int) -> float: 
+   for year in range(years): 
+     principal = ((1 + (percentage/100)) * principal)
+   return principal 
 print(compound_interest(5, 1000, 5))
 
 #Hollow Square
@@ -64,4 +62,4 @@ def draw_hollow_square(height, thickness):
     draw_square_middle(height, thickness)
     draw_square_ends(height, thickness)
 
-draw_hollow_square(8, 2)
+draw_hollow_square(13, 3)
